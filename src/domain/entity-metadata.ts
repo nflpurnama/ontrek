4dbe36d12d65df2aba0e6025f8aef6d59eabe220
nflpurnama <purnama.naufal.adli@gmail.com>
2026-02-11 20:08:43 +0700

    [DEV] create metadata domain class

---
+++ src/domain/entity-metadata.ts
@@ -0,0 +1,47 @@
+export class EntityMetadata{
+    private constructor(
+        protected readonly _id: string,
+        protected readonly _createdAt: Date,
+        protected _updatedAt: Date
+    ){}
+
+    get id(){
+        return this._id;
+    }
+
+    get createdAt(){
+        return this._createdAt;
+    }
+
+    get updatedAt(){
+        return this._updatedAt;
+    }
+
+    static generateNow(){
+        return new Date();
+    }
+
+    static generateUUID(){
+        return crypto.randomUUID();
+    }
+
+    touch(){
+        this._updatedAt = EntityMetadata.generateNow()
+    }
+
+    static create(): EntityMetadata{
+        return new EntityMetadata(EntityMetadata.generateUUID(), EntityMetadata.generateNow(), EntityMetadata.generateNow());
+    }
+
+    static rehydrate(params: {
+        id: string,
+        createdAt: Date,
+        updatedAt: Date
+    }){
+        return new EntityMetadata(
+            params.id,
+            params.createdAt,
+            params.updatedAt
+        )
+    }
+}
