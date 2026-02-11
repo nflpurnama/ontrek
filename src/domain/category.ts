56f8393e121d88637718e0b3e4eae64eed62c658
nflpurnama <purnama.naufal.adli@gmail.com>
2026-02-11 20:31:44 +0700

    [DEV] create category class

---
+++ src/domain/category.ts
@@ -0,0 +1,59 @@
+import { EntityMetadata } from "./entity-metadata";
+
+class Category {
+  private constructor(
+    private _name: string,
+    private readonly _metadata: EntityMetadata,
+  ) {}
+
+  static create(params: { name: string }) {
+    return new Category(
+        Category.formatName(params.name), EntityMetadata.create());
+  }
+
+  static rehydrate(params: {
+    name: string;
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+  }) {
+    return new Category(
+      params.name,
+      EntityMetadata.rehydrate({
+        id: params.id,
+        createdAt: params.createdAt,
+        updatedAt: params.updatedAt,
+      }),
+    );
+  }
+
+  private static formatName(name: string){
+    const trimmed = name.trim();
+    if (!trimmed) {
+      throw Error("Category name cannot be empty.");
+    }
+
+    return trimmed;
+  }
+
+  get id() {
+    return this._metadata.id;
+  }
+
+  get createdAt() {
+    return this._metadata.createdAt;
+  }
+
+  get updatedAt() {
+    return this._metadata.updatedAt;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  rename(newValue: string) {
+    this._name = Category.formatName(newValue);
+    this._metadata.touch();
+  }
+}
