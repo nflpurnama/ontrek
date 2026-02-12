d516237e1feaac57bc60d4eceef6e5262a065640
nflpurnama <purnama.naufal.adli@gmail.com>
2026-02-12 18:56:32 +0700

    [DEV] create account class

--- TODO.md
+++ TODO.md
@@ -1,2 +1,2 @@
-1. create account object
 2. set ID as value object or type instead of string
+3. create money value object
---
+++ src/domain/account.ts
@@ -0,0 +1,88 @@
+import { EntityMetadata } from "./entity-metadata";
+
+export class Account {
+  private constructor(
+    private readonly _metadata: EntityMetadata,
+    private _name: string,
+    private _balance: number,
+  ) {}
+
+  private static formatName(name: string) {
+    const trimmed = name.trim();
+
+    if (!trimmed) {
+      throw new Error("Account name cannot be empty.");
+    }
+
+    return trimmed;
+  }
+
+  static create(params: { name: string; balance: number }) {
+    return new Account(
+      EntityMetadata.create(),
+      Account.formatName(params.name),
+      params.balance,
+    );
+  }
+
+  static rehydrate(params: {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    name: string;
+    balance: number;
+  }) {
+    return new Account(
+      EntityMetadata.rehydrate({
+        id: params.id,
+        createdAt: params.createdAt,
+        updatedAt: params.updatedAt,
+      }),
+      Account.formatName(params.name),
+      params.balance,
+    );
+  }
+
+  get id() {
+    return this._metadata.id;
+  }
+
+  get createdAt() {
+    return this._metadata.createdAt;
+  }
+
+  get updatedAt() {
+    return this._metadata.updatedAt;
+  }
+
+  get balance() {
+    return this._balance;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  rename(newName: string) {
+    this._name = Account.formatName(newName);
+    this._metadata.touch();
+  }
+
+  credit(amount: number) {
+    if (!Number.isFinite(amount) || amount < 0) {
+      throw new Error("Credit amount must be positive.");
+    }
+
+    this._balance += amount;
+    this._metadata.touch();
+  }
+
+  debit(amount: number) {
+    if (!Number.isFinite(amount) || amount < 0) {
+      throw new Error("Debit amount must be positive.");
+    }
+
+    this._balance -= amount;
+    this._metadata.touch();
+  }
+}
