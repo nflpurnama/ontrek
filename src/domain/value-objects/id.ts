6b8435f2a28a6136f47f10bd056e9fe62fca3665
nflpurnama <purnama.naufal.adli@gmail.com>
2026-02-14 09:27:00 +0700

    [DEV] create id value object

---
+++ src/domain/value-objects/id.ts
@@ -0,0 +1,31 @@
+export class Id {
+  private constructor(private readonly value: string) {}
+
+  private static isValidUUID(value: string): boolean {
+    const uuidRegex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(value);
+  }
+
+  public static rehydrate(value: string): Id {
+    if (!value || value.trim().length === 0) {
+      throw new Error("AccountId cannot be empty");
+    } else if (!Id.isValidUUID(value)) {
+        throw new Error("AccountId must be UUID");
+    }
+
+    return new Id(value);
+  }
+
+  public static create(): Id {
+    return new Id(crypto.randomUUID());
+  }
+
+  public getValue(): string {
+    return this.value;
+  }
+
+  public equals(other: Id): boolean {
+    return this.value === other.value;
+  }
+}
