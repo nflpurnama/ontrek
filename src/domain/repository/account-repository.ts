2ae1aecf92e6bb56119006e9eb32dc378e42c472
nflpurnama <purnama.naufal.adli@gmail.com>
2026-02-15 20:04:25 +0700

    [DEV] update account repository

--- src/domain/repository/account-repository.ts
+++ src/domain/repository/account-repository.ts
@@ -2,7 +2,8 @@ import { Account } from "../entities/account";
 import { Id } from "../value-objects/id";
 
 export interface AccountRepository {
-  getAccounts(ids: Id[]): Promise<Account[]>;
+  get(ids: Id[]): Promise<Account[]>;
+  getAll(): Promise<Account[]>;
   save(account: Account): Promise<Id>;
   update(account: Account): Promise<Id>;
   delete(id: Id): Promise<void>;
