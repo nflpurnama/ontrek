521b24ff0f07e0799eef0902d0d3f53dcb042065
nflpurnama <purnama.naufal.adli@gmail.com>
2026-02-15 20:12:05 +0700

    [DEV] use sqliteprovider in app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
-import { Stack } from "expo-router";
+import { SQLiteProvider } from "expo-sqlite";
+import { SQLITE_DB_NAME } from "@/src/config/database";
+import { initializeDatabase } from "@/src/infrastructure/database/sqlite/init";
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SQLiteProvider
+      databaseName={SQLITE_DB_NAME}
+      onInit={initializeDatabase}
+    >
+      <Slot />
+    </SQLiteProvider>
+  );
 }
