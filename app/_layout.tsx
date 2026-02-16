4ed5bc02f3eeb644f9b17019a4c2c9555e827ff2
nflpurnama <purnama.naufal.adli@gmail.com>
2026-02-16 21:27:59 +0700

    [DEV] fetch dependency

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,29 @@
-import { SQLiteProvider } from "expo-sqlite";
-import { SQLITE_DB_NAME } from "@/src/config/database";
-import { initializeDatabase } from "@/src/infrastructure/database/sqlite/init";
 import { Slot } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
+
+import { Dependencies, DependencyProvider } from "@/src/application/providers/dependency-provider";
+import { createDependencies } from "@/src/infrastructure/container/dependency-container";
 
 export default function RootLayout() {
+  const [deps, setDeps] = useState<Dependencies | null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      const created = await createDependencies();
+      setDeps(created);
+    };
+
+    init();
+  }, []);
+
+  if (!deps) {
+    return <ActivityIndicator />;
+  }
+
   return (
-    <SQLiteProvider
-      databaseName={SQLITE_DB_NAME}
-      onInit={initializeDatabase}
-    >
-      <Slot />
-    </SQLiteProvider>
+      <DependencyProvider dependencies={deps}>
+        <Slot />
+      </DependencyProvider>
   );
 }
