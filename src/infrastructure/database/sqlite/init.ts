86256eaf936c0ca60957f490d5ddc39b51b3ee71
nflpurnama <purnama.naufal.adli@gmail.com>
2026-02-18 05:35:09 +0700

    [DEV] register transactions table in sqlite db init

--- src/infrastructure/database/sqlite/init.ts
+++ src/infrastructure/database/sqlite/init.ts
@@ -1,6 +1,8 @@
 import { SQLiteDatabase } from 'expo-sqlite';
 import { ACCOUNTS_TABLE_INIT_QUERY } from './schema/accounts';
+import { TRANSACTIONS_TABLE_INIT_QUERY } from './schema/transaction';
 
 export async function initializeDatabase(db: SQLiteDatabase) {
   await db.execAsync(ACCOUNTS_TABLE_INIT_QUERY);
+  await db.execAsync(TRANSACTIONS_TABLE_INIT_QUERY)
 }
