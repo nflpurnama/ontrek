958689cae3d802139a5fe549a6feaa7d6b791bda
Naufal Purnama <purnama.naufal.adli@gmail.com>
2026-02-26 21:15:22 +0700

    feat(sqlite-transaction): add spending_type column

--- src/infrastructure/database/sqlite/schema/transaction.ts
+++ src/infrastructure/database/sqlite/schema/transaction.ts
@@ -4,7 +4,8 @@ export const TRANSACTIONS_TABLE_INIT_QUERY = `
     vendor_id TEXT,
     category_id TEXT,
     transaction_date TEXT NOT NULL,
-    transaction_type INTEGER NOT NULL,
+    transaction_type TEXT NOT NULL,
+    spending_type TEXT NOT NULL,
     amount INTEGER NOT NULL,
     description TEXT,
     created_at TEXT NOT NULL,
@@ -17,9 +18,10 @@ export interface SqliteTransaction {
   vendor_id: string;
   category_id: string;
   transaction_date: string;
-  transaction_type: number;
+  transaction_type: string;
+  spending_type: string;
   amount: number;
-  description?: string;
+  description: string | null;
   created_at: string;
   updated_at: string;
 }
