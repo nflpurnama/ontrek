107fd36eb1d25aa3e27317a7e9c54b60c499080f
Naufal Purnama <purnama.naufal.adli@gmail.com>
2026-02-28 22:51:39 +0700

    feat(dependency-provider): register new use case types
    feat(dependency-container): register new use cases

--- src/application/providers/dependency-provider.tsx
+++ src/application/providers/dependency-provider.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { GetDashboardUseCase } from "../use-case/account/get-dashboard.usecase";
 import { CreateTransactionUseCase } from "../use-case/transaction/create-transaction";
-import { ViewTransactionsUseCase } from "../use-case/transaction/view-transaction";
 import { DeleteTransactionUseCase } from "../use-case/transaction/delete-transaction";
+import { ViewTransactionsUseCase } from "../use-case/transaction/view-transaction";
 import { CreateVendorUseCase } from "../use-case/vendor/create-vendor";
 import { FindVendorsUseCase } from "../use-case/vendor/find-vendors";
+import { GetAllCategoriesUseCase } from "../use-case/category/get-all-categories";
+import { EnsureDefaultAccountUseCase } from "../use-case/account/ensure-default-account";
+import { EnsureDefaultCategoriesUseCase } from "../use-case/category/ensure-default-categories";
 
+//TODO: explore if dependency is importing too many things
 export interface Dependencies {
-    getDashboardUseCase: GetDashboardUseCase;
-    createTransactionUseCase: CreateTransactionUseCase;
-    deleteTransactionUseCase: DeleteTransactionUseCase;
-    viewTransactionsUseCase: ViewTransactionsUseCase;
-    createVendorUseCase: CreateVendorUseCase;
-    findVendorsUseCase: FindVendorsUseCase;
-  }
+  ensureDefaultAccountUseCase: EnsureDefaultAccountUseCase,
+  ensureDefaultCategoriesUseCase: EnsureDefaultCategoriesUseCase,
+  getDashboardUseCase: GetDashboardUseCase;
+  createTransactionUseCase: CreateTransactionUseCase;
+  deleteTransactionUseCase: DeleteTransactionUseCase;
+  viewTransactionsUseCase: ViewTransactionsUseCase;
+  createVendorUseCase: CreateVendorUseCase;
+  findVendorsUseCase: FindVendorsUseCase;
+  getAllCategoriesUseCase: GetAllCategoriesUseCase;
+}
 
 export const DependencyContext = React.createContext<Dependencies | null>(null);
 
--- src/infrastructure/container/dependency-container.ts
+++ src/infrastructure/container/dependency-container.ts
@@ -1,21 +1,21 @@
-import * as SQLite from "expo-sqlite";
+import { Dependencies } from "@/src/application/providers/dependency-provider";
+import { EnsureDefaultAccountUseCase } from "@/src/application/use-case/account/ensure-default-account";
+import { GetDashboardUseCase } from "@/src/application/use-case/account/get-dashboard.usecase";
+import { CreateTransactionUseCase } from "@/src/application/use-case/transaction/create-transaction";
+import { DeleteTransactionUseCase } from "@/src/application/use-case/transaction/delete-transaction";
+import { ViewTransactionsUseCase } from "@/src/application/use-case/transaction/view-transaction";
+import { CreateVendorUseCase } from "@/src/application/use-case/vendor/create-vendor";
+import { FindVendorsUseCase } from "@/src/application/use-case/vendor/find-vendors";
 import { SQLITE_DB_NAME } from "@/src/config/database";
+import * as SQLite from "expo-sqlite";
 import { initializeDatabase } from "../database/sqlite/init";
+import { SqliteTransaction } from "../database/sqlite/sqlite-transaction";
 import { SqliteAccountRepository } from "../repository/sqlite/account-repository";
 import { SqliteTransactionRepository } from "../repository/sqlite/transaction-repository";
 import { SqliteVendorRepository } from "../repository/sqlite/vendor-repository";
 import { SqliteFinancialTransactionService } from "../services/sqlite/sqlite-financial-transaction-service";
-import { SqliteTransaction } from "../database/sqlite/sqlite-transaction";
-import { Dependencies } from "@/src/application/providers/dependency-provider";
-import { GetDashboardUseCase } from "@/src/application/use-case/account/get-dashboard.usecase";
-import { EnsureDefaultAccountUseCase } from "@/src/application/use-case/account/ensure-default-account";
-import { CreateTransactionUseCase } from "@/src/application/use-case/transaction/create-transaction";
-import { ViewTransactionsUseCase } from "@/src/application/use-case/transaction/view-transaction";
-import { DeleteTransactionUseCase } from "@/src/application/use-case/transaction/delete-transaction";
-import { CreateVendorUseCase } from "@/src/application/use-case/vendor/create-vendor";
-import { FindVendorsUseCase } from "@/src/application/use-case/vendor/find-vendors";
 
-import { drizzle } from "drizzle-orm/expo-sqlite"
+import { GetAllCategoriesUseCase } from "@/src/application/use-case/category/get-all-categories";
 import { SqliteCategoryRepository } from "../repository/sqlite/category-repository";
 import { EnsureDefaultCategoriesUseCase } from "@/src/application/use-case/category/ensure-default-categories";
 
@@ -30,27 +30,52 @@ export async function createDependencies(db: SQLite.SQLiteDatabase, drizzleDb: E
   const vendorRepository = new SqliteVendorRepository(db);
   const categoryRepository = new SqliteCategoryRepository(drizzleDb);
 
-  const ensureDefaultAccountUseCase = new EnsureDefaultAccountUseCase(accountRepository);
+  const ensureDefaultAccountUseCase = new EnsureDefaultAccountUseCase(
+    accountRepository,
+  );
+  const ensureDefaultCategoriesUseCase = new EnsureDefaultCategoriesUseCase(
+    categoryRepository,
+  );
+
   await ensureDefaultAccountUseCase.execute();
+  await ensureDefaultCategoriesUseCase.execute();
 
   const getDashboardUseCase = new GetDashboardUseCase(accountRepository);
 
   const sqliteTransaction = new SqliteTransaction(db);
-  const financialTransactionService = new SqliteFinancialTransactionService(sqliteTransaction, accountRepository, transactionRepository, vendorRepository);
-  const createTransactionUseCase = new CreateTransactionUseCase(financialTransactionService);
-  const deleteTransactionUseCase = new DeleteTransactionUseCase(financialTransactionService);
+  const financialTransactionService = new SqliteFinancialTransactionService(
+    sqliteTransaction,
+    accountRepository,
+    transactionRepository,
+    vendorRepository,
+  );
+  const createTransactionUseCase = new CreateTransactionUseCase(
+    financialTransactionService,
+  );
+  const deleteTransactionUseCase = new DeleteTransactionUseCase(
+    financialTransactionService,
+  );
 
-  const viewTransactionsUseCase = new ViewTransactionsUseCase(transactionRepository);
+  const viewTransactionsUseCase = new ViewTransactionsUseCase(
+    transactionRepository,
+  );
 
   const createVendorUseCase = new CreateVendorUseCase(vendorRepository);
   const findVendorsUseCase = new FindVendorsUseCase(vendorRepository);
 
+  const getAllCategoriesUseCase = new GetAllCategoriesUseCase(
+    categoryRepository,
+  );
+
   return {
+    ensureDefaultAccountUseCase,
+    ensureDefaultCategoriesUseCase,
     getDashboardUseCase,
     createTransactionUseCase,
     deleteTransactionUseCase,
     viewTransactionsUseCase,
     createVendorUseCase,
-    findVendorsUseCase
+    findVendorsUseCase,
+    getAllCategoriesUseCase,
   };
 }
