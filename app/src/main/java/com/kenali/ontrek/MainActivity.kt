c8f3ec0824f12e3b8a273883f84bd5550dd39fc9
Naufal Purnama <purnama.naufal.adli@gmail.com>
2026-07-10 10:50:08 +0700

    Initial commit: Android Studio scaffold with MainActivity

---
+++ app/src/main/java/com/kenali/ontrek/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kenali.ontrek
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textView = TextView(this)
+        textView.text = "Hello Ontrek!"
+        textView.textSize = 24f
+        setContentView(textView)
+    }
+}
